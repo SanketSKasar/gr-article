4a1f523cce1559b9235b8fe14e4fcb42ca1b4bb5
SanketSKasar <kasar.sanket.s@gmail.com>
2020-02-01 15:26:17 +0530

    added migrations

--- gr_article/core/models.py
+++ gr_article/core/models.py
@@ -3,17 +3,16 @@ import qrcode
 
 from .constants import *
 
-class GR(models.Model):
-    date = models.DateField(auto_now_add=True)
-    supplier = models.ForeignKey(Supplier)
-    goods = models.ForeignKey()
+class Article(models.Model):
+    name = models.CharField(max_length = 50)
+    unit = models.CharField(max_length = 2, choices=units_of_measurement, null=True, blank=True)
 
     def __str__(self):
-        return f"{self.date}-{self.supplier.name}"
+        return self.name
 
 class Goods(models.Model):
     sr_no = models.IntegerField()
-    article = models.ForeignKey(Article)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     color = models.CharField(max_length = 20, choices = article_colors)
     other = models.TextField()
@@ -24,13 +23,6 @@ class Goods(models.Model):
     def get_qr(self):
         return qrcode.make(f"{self.article.name}-{self.article.unit}-{self.color}-{self.quantity}")
 
-class Article(models.Model):
-    name = models.CharField(max_length = 50)
-    unit = models.CharField(max_length = 2, choices=units_of_measurement, null=True, blank=True)
-
-    def __str__(self):
-        return self.name
-
 class Supplier(models.Model):
     name =  models.CharField(max_length = 100)
     address = models.CharField(max_length = 200)
@@ -39,4 +31,12 @@ class Supplier(models.Model):
 
     def __str__(self):
         return f"{self.name}-{self.gst_no}"
+
+class GR(models.Model):
+    date = models.DateField(auto_now_add=True)
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    goods = models.ForeignKey(Goods, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.date}-{self.supplier.name}"
 
